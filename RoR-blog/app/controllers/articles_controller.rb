e0de627ecb4ac1263b1c9413c6eb37871c58eb88
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-05-15 15:57:32 +0300

    Add status column to articles and comments
    This commit adds a `status` column to the `articles` and `comments` tables in the database. The `AddStatusToArticles` and `AddStatusToComments` migrations are created to add the `status` column to the respective tables.
    
    The `status` column is of type `string` and is used to track the visibility status of articles and comments. The `Visible` concern is updated to include the `VALID_STATUSES` constant and a validation for the `status` attribute.
    
    These changes allow for better control over the visibility of articles and comments in the application.

--- RoR-blog/app/controllers/articles_controller.rb
+++ RoR-blog/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
   
   def index
     @articles = Article.all
@@ -43,7 +44,7 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :status)
   end
 
 end
